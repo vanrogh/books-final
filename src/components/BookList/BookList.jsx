012e83d3b5691fd304fcf50c55cd65bda563d89f
vanrogh <k.elaman2002@gmail.com>
2024-01-19 16:59:30 +0600

    Add search by name of the book

--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,17 +1,16 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import './BookList.css';
-import {API_URL} from '../../API';
+import { API_URL } from '../../API';
 import axios from 'axios';
 import { useAppContext } from '../context/appContext';
 import { useNavigate } from 'react-router-dom';
 
 const BookList = () => {
     const [books, setBooks] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
 
     const { favorites, addToFavorites, removeFromFavorites } = useAppContext();
-
     const navigate = useNavigate();
-
 
     const favoritesChecker = (id) => {
         const boolean = favorites.some((book) => book.id === id);
@@ -21,38 +20,58 @@ const BookList = () => {
     useEffect(() => {
         axios
             .get(API_URL)
-            .then(res => {
+            .then((res) => {
                 console.log(res.data);
                 setBooks(res.data);
-        })
-        .catch(err=>console.log(err));
+            })
+            .catch((err) => console.log(err));
     }, []);
 
-  return (
-   <div className='book-list'>
-    {books.map((book) => (
-        <div key={book.id} className='book'>
-            <div>
-                <h4>{book.title}</h4>
-            </div>
-            <div>
-                <img src={book.image_url} alt="#" onClick={() => navigate(`/books/${book.id}`)} />
-            </div>
-            <div>
-                {favoritesChecker(book.id) ? (
-                    <button onClick={()=> removeFromFavorites(book.id)}>
-                        Удалить из Избранных
-                    </button> 
-                ) : (
-                    <button onClick={()=> addToFavorites(book)}>
-                        Добавить в Избранные
-                    </button>
-                )}
+    const handleSearch = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const filteredBooks = books.filter((book) =>
+        book.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+    return (
+        <div className='book-list'>
+            <input
+                type="text"
+                placeholder="Поиск по названию книги"
+                value={searchQuery}
+                onChange={handleSearch}
+            />
+            <div className='book-container'>
+                {filteredBooks.map((book) => (
+                    <div key={book.id} className='book'>
+                        <div>
+                            <h4>{book.title}</h4>
+                        </div>
+                        <div>
+                            <img
+                                src={book.image_url}
+                                alt="#"
+                                onClick={() => navigate(`/books/${book.id}`)}
+                            />
+                        </div>
+                        <div>
+                            {favoritesChecker(book.id) ? (
+                                <button onClick={() => removeFromFavorites(book.id)}>
+                                    Удалить из Избранных
+                                </button>
+                            ) : (
+                                <button onClick={() => addToFavorites(book)}>
+                                    Добавить в Избранные
+                                </button>
+                            )}
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
-    ))}
-  </div>
-  );
-}
+    );
+};
 
-export default BookList
+export default BookList;
